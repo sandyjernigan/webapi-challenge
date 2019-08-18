cf38be608032fe848987c28c295256c42c8c1821
Sandy <sandy.jernigan@gmail.com>
2019-08-18 19:45:23 -0400

    Middleware

--- api/server.js
+++ api/server.js
@@ -11,7 +11,20 @@ const ActionsRouter = require('./ActionsRouter.js');
 server.use('/api/projects', ProjectsRouter);
 server.use('/api/actions', ActionsRouter);
 
+// Middleware
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+//custom middleware
+function logger(req, res, next) {
+  const timeStamp = new Date().toUTCString();
+  console.log(`Method: ${req.method} URL: ${req.url} Timestamp: ${timeStamp}`);
+  next();
+};
+
 // Base Folder
 server.get('/', (req, res) => {
   res.send(`<h2>Hello World!</h2><p><a href="./api/projects">Projects</a></p><p><a href="./api/actions">Actions</a></p>`)
 });
+
