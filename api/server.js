b91414b9ee476610102ade1d16e6e4d9030b7f9c
Sandy <sandy.jernigan@gmail.com>
2019-08-18 19:44:25 -0400

    Base Folder

--- api/server.js
+++ api/server.js
@@ -10,3 +10,8 @@ const ProjectsRouter = require('./ProjectsRouter.js');
 const ActionsRouter = require('./ActionsRouter.js');
 server.use('/api/projects', ProjectsRouter);
 server.use('/api/actions', ActionsRouter);
+
+// Base Folder
+server.get('/', (req, res) => {
+  res.send(`<h2>Hello World!</h2><p><a href="./api/projects">Projects</a></p><p><a href="./api/actions">Actions</a></p>`)
+});
