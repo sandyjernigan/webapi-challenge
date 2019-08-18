a4e4e20a0f7328546071f2ec7571495ab59efc08
Sandy <sandy.jernigan@gmail.com>
2019-08-18 19:55:07 -0400

    Setup base ActionsRouter file

---
+++ api/ActionsRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Database
+const ActionsDB = require('../data/helpers/actionModel.js');
+
+//#region - CREATE 
+//#endregion
+
+//#region - READ
+//#endregion
+
+//#region - UPDATE
+//#endregion
+
+//#region - DELETE
+//#endregion
+
+module.exports = router;
