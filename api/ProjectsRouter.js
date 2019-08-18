4a4b93d5bdacbccaaab0b7c23059c2306a8a4fe6
Sandy <sandy.jernigan@gmail.com>
2019-08-18 19:54:20 -0400

    set regions

--- api/ProjectsRouter.js
+++ api/ProjectsRouter.js
@@ -4,6 +4,16 @@ const router = express.Router();
 // Database
 const ProjectsDB = require('../data/helpers/projectModel.js');
 
+//#region - CREATE 
+//#endregion
 
+//#region - READ
+//#endregion
+
+//#region - UPDATE
+//#endregion
+
+//#region - DELETE
+//#endregion
 
 module.exports = router;
