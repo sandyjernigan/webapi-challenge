de9b1c33159c1d5c8f301d6f865e27e744eead42
Sandy <sandy.jernigan@gmail.com>
2019-08-18 20:02:26 -0400

    Read All - Returns an array of all projects

--- api/ProjectsRouter.js
+++ api/ProjectsRouter.js
@@ -2,18 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 // Database
-const ProjectsDB = require('../data/helpers/projectModel.js');
+const DB = require('../data/helpers/projectModel.js');
 
 //#region - CREATE 
+  // `insert()`: calling insert passing it a resource object will add it to the database and return the newly created resource.
+
 //#endregion
 
 //#region - READ
+  // `get()`: calling get returns an array of all the resources contained in the database. If you pass an `id` to this method it will return the resource with that id if one is found.
+
+// Read All - Returns an array of all projects
+router.get('/', async (req, res, next) => {
+  try {
+    const results = await DB.get();
+    res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "The information could not be retrieved." });
+  }
+});
+
+  // The `projectModel.js` helper includes an extra method called `getProjectActions()` that takes a _project id_ as it's only argument and returns a list of all the _actions_ for the _project_.
+
 //#endregion
 
-//#region - UPDATE
+//#region - UPDATE 
+  // `update()`: accepts two arguments, the first is the `id` of the resource to update, and the second is an object with the `changes` to apply. It returns the updated resource. If a resource with the provided `id` is not found, the method returns `null`.
 //#endregion
 
-//#region - DELETE
+//#region - DELETE 
+  // `remove()`: the remove method accepts an `id` as it's first parameter and, upon successfully deleting the resource from the database, returns the number of records deleted.
 //#endregion
 
 module.exports = router;
